1eedd18fd0ea8e251d4dae9d8f1a7a00ea573a3f
David Young <dyoung.2019.github@gmail.com>
2021-04-15 18:45:51 +1000

    changed commonjs building to webpack; removed snowpack & html

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var WebpackCleanPlugin = require('webpack-clean');
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: { 
+          configFile: 'tsconfig-cjs.json',
+          onlyCompileBundledFiles: true
+        }
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },  
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index.js',
+    clean: true
+  },
+};
